6109bf6c0ec1dc93023f011866df403f790f0c1d
Fsesnick <61622322+Fsesnick@users.noreply.github.com>
2021-05-13 22:22:46 -0300

    12 profiles with the html markup provided

--- script.js
+++ script.js
@@ -21,14 +21,17 @@ fetch('https://randomuser.me/api/?results=12&nat=br&lego')
 function generateProfile(data){
 
     const cards = data.map( profile =>`
-    <img src='${profile.picture.medium}' alt>
-    <p> CLick to view ${profile.name.first} ${profile.name.last}</p>
+    <div class="card">
+    <div class="card-img-container">
+        <img class="card-img" src="${profile.picture.medium}" alt="profile picture">
+    </div>
+    <div class="card-info-container">
+        <h3 id="name" class="card-name cap">${profile.name.first} ${profile.name.last}</h3>
+        <p class="card-text">${profile.email}</p>
+        <p class="card-text cap">${profile.location.city}, ${profile.location.state}</p>
+    </div>
+</div>
     ` );
-
-  /*  const html =`
-    <img src='${data.results[0].picture.medium}' alt>
-    <p> CLick to view ${data.results[0].name.first} ${data.results[0].name.last}</p>
-    `;
-    */
+    
     gallery.innerHTML = cards;
 }
